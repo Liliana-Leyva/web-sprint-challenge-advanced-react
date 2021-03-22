37cfb5da2ef6edc777c47789cd2dfb2415916ed0
Liliana Leyva <ingeleyva@yahoo.com>
2021-03-21 22:42:24 -0500

    doing the test

--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,9 +1,23 @@
 import React from "react";
 import { render } from "@testing-library/react";
 import CheckoutForm from "./CheckoutForm";
+import userEvent from '@testing-library/user-event'
 
 // Write up the two tests here and make sure they are testing what the title shows
+
+//testing the checkout form
+test("CheckoutForm", ()=> {
+    render(<CheckoutForm errors={[]}/>);
+})
+
 
 test("form header renders", () => {});
 
 test("form shows success message on submit with form details", () => {});
+
+
+test ("CheckoutForm adds the inputs data when the form is submitted", ()=> {
+    render(<CheckoutForm errors={[
+    ]}/>);
+    
+} )
